93301ef1615a157985f649e6eea8c04814973dd1
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-18 14:42:25 -0500

    Update kernel.c

--- cola/kernel.c
+++ cola/kernel.c
@@ -4,3 +4,19 @@
     Programmer : Aether
     File : kernel.c
 */
+
+#include <kernel.h>
+
+int get_kernel_current_state(struct kernel* k) {
+    if( k->state == KERNEL_SLEEPING ) {
+        return KERNEL_SLEEPING;
+    }
+    if( k->state == KERNEL_ACTIVE ) {
+        return KERNEL_ACTIVE;
+    }
+    if( k->state == KERNEL_BOOTING ) {
+        return KERNEL_BOOTING;
+    }
+    
+  return k->state;
+}
